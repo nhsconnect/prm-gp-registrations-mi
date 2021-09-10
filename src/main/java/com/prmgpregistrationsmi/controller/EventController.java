0cf2b3627cf1497c184f5e6b1ae8a933d9b3ba72
Dorota-TW <dorota.sobkow1@nhs.net>
2021-09-10 15:49:32 +0100

    PRMT-2341 [AB/JN/DS] Return request body in POST /event request

--- src/main/java/com/prmgpregistrationsmi/controller/EventController.java
+++ src/main/java/com/prmgpregistrationsmi/controller/EventController.java
@@ -1,17 +1,21 @@
 package com.prmgpregistrationsmi.controller;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EventController {
 
-    @PostMapping("/event")
-    @ResponseStatus(HttpStatus.OK)
-    public void transferEvent() {
-        System.out.println("Hello world");
+    @PostMapping(
+            value = "/event",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public String transferEvent(@RequestBody String body) {
+        System.out.println("POST request to /event endpoint with body: " + body);
+        return body;
     }
 }
 
--- src/test/java/com/prmgpregistrationsmi/controller/EventControllerTest.java
+++ src/test/java/com/prmgpregistrationsmi/controller/EventControllerTest.java
@@ -3,9 +3,11 @@ package com.prmgpregistrationsmi.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(EventController.class)
@@ -16,6 +18,9 @@ class EventControllerTest {
 
     @Test
     void shouldPostAnEvent() throws Exception {
-        mockMvc.perform(post("/event")).andExpect(status().isOk());
+        String requestBody = "{'event':'banana'}";
+        mockMvc.perform(post("/event").content(requestBody).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(requestBody));
     }
 }
