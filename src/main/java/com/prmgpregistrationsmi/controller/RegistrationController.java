547e9b3b7b6ea320cd6c6adb6325bddfba8c117b
Jake Newman <jacob.newman1@nhs.net>
2021-09-30 17:08:15 +0100

    PRMT-2379 [JN/JF/KV] Add gp2gpRegistrationStarted endpoint with validation of registrationId in path

--- src/main/java/com/prmgpregistrationsmi/controller/RegistrationController.java
+++ src/main/java/com/prmgpregistrationsmi/controller/RegistrationController.java
@@ -1,18 +1,27 @@
 package com.prmgpregistrationsmi.controller;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@RequestMapping("registration")
 @RestController
+@RequestMapping("registration")
+@Validated
 public class RegistrationController {
     @PostMapping(
             value = "/{registrationId}/gp2gpRegistrationStarted",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public String transferEvent(@RequestBody String body, @PathVariable String registrationId) {
-        System.out.printf("POST request to: %s/gp2gpRegistrationStarted endpoint with body: %s", registrationId, body);
-        return body;
+    public String registrationStartedEvent(
+            @PathVariable @Length(min = 4, max = 32) String registrationId,
+            @RequestBody String body) {
+                System.out.printf("POST request to: %s/gp2gpRegistrationStarted endpoint with body: %s", registrationId, body);
+                return body;
     }
 }
--- src/test/java/com/prmgpregistrationsmi/controller/RegistrationControllerTest.java
+++ src/test/java/com/prmgpregistrationsmi/controller/RegistrationControllerTest.java
@@ -1,11 +1,14 @@
 package com.prmgpregistrationsmi.controller;
 
+import io.micrometer.core.instrument.config.validate.ValidationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -17,10 +20,31 @@ class RegistrationControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    void shouldPostAnEvent() throws Exception {
+    void shouldReturnRequestBodyInResponse() throws Exception {
         String requestBody = "{'event':'banana'}";
-        mockMvc.perform(post("/registration/1234/gp2gpRegistrationStarted").content(requestBody).contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(post("/registration/12345/gp2gpRegistrationStarted").content(requestBody).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().json(requestBody));
+    }
+
+    @Test()
+    void shouldReturnA400IfRegistrationIDIsLessThan4Characters() throws Exception {
+        String requestBody = "{'event':'banana'}";
+        mockMvc.perform(post("/registration/1/gp2gpRegistrationStarted").content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof Exception))
+                .andExpect(result -> assertEquals("registrationStartedEvent.registrationId: length must be between 4 and 32", result.getResolvedException().getMessage()));
+    }
+
+
+    @Test()
+    void shouldReturnA400IfRegistrationIDIsMoreThan32Characters() throws Exception {
+        String requestBody = "{'event':'banana'}";
+        mockMvc.perform(post("/registration/000000000011111111112222222222333/gp2gpRegistrationStarted").content(requestBody)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof Exception))
+                .andExpect(result -> assertEquals("registrationStartedEvent.registrationId: length must be between 4 and 32", result.getResolvedException().getMessage()));
     }
 }
