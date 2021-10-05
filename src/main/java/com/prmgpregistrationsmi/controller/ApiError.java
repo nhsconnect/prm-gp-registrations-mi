1b0be44d04060b8cd2d030f828a538ab5f71e97b
Jake Newman <jacob.newman1@nhs.net>
2021-10-05 14:50:53 +0100

    PRMT-2379 [JN] Fix ApiError spotbugs by only adding getter on status and also creating a new list of errors

--- src/main/java/com/prmgpregistrationsmi/controller/ApiError.java
+++ src/main/java/com/prmgpregistrationsmi/controller/ApiError.java
@@ -1,28 +1,31 @@
 package com.prmgpregistrationsmi.controller;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-@Getter
-public class ApiError {
-    private HttpStatus status;
-    private String message;
-    private List<String> errors;
+public final class ApiError {
+    private final HttpStatus status;
+    private final String message;
+    private final List<String> errors;
 
     public ApiError(HttpStatus status, String message, List<String> errors) {
         super();
         this.status = status;
         this.message = message;
-        this.errors = errors;
+        this.errors = new ArrayList<>(errors);
     }
 
     public ApiError(HttpStatus status, String message, String error) {
         super();
         this.status = status;
         this.message = message;
-        errors = Arrays.asList(error);
+        this.errors = Collections.singletonList(error);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
     }
 }
