a3e2cc702cda322d9eb37c1ef2f49685a786ac24
helen.zhou <helen.zhou1@nhs.net>
2021-11-29 10:41:24 +0000

    PRMT-2445 [HZ/KV] Move integration tests related to GlobalExceptionHandler to separate file

---
+++ src/integrationtest/java/com/prmgpregistrationsmi/GlobalExceptionHandlerIntegrationTest.java
@@ -0,0 +1,106 @@
+package com.prmgpregistrationsmi;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
+import com.prmgpregistrationsmi.controller.ApiError;
+import com.prmgpregistrationsmi.controller.RegistrationController;
+import com.prmgpregistrationsmi.model.RegistrationStartedEvent;
+import com.prmgpregistrationsmi.service.RegistrationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static com.prmgpregistrationsmi.controller.RegistrationController.API_VERSION;
+import static com.prmgpregistrationsmi.utils.JsonHelper.asJsonString;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(RegistrationController.class)
+class GlobalExceptionHandlerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private RegistrationService mockRegistrationService;
+
+    @Test
+    void shouldReturn400RequestBodyIsEmpty() throws Exception {
+        RegistrationStartedEvent emptyRequestBody = new RegistrationStartedEvent();
+
+        ApiError expectedResponse = new ApiError(
+                HttpStatus.BAD_REQUEST,
+                "Failed to validate fields", List.of(
+                "reportingPracticeOdsCode: must not be empty",
+                "eventGeneratedTimestamp: must not be null",
+                "registrationId: must not be empty",
+                "payload: must not be null",
+                "reportingSystemSupplier: must not be empty",
+                "eventId: must not be empty"));
+
+        mockMvc.perform(post("/registration/" + API_VERSION + "/gp2gpRegistrationStarted").content(asJsonString(emptyRequestBody))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException))
+                .andExpect(content().json(asJsonString(expectedResponse)));
+    }
+
+    @Test
+    void shouldReturnA400IfEventGeneratedTimestampIsNotANumber() throws Exception {
+        var requestBody = "{\"eventId\":\"some-id\",\"eventGeneratedTimestamp\":" + false + "}";
+
+        ApiError expectedResponse = new ApiError(
+                HttpStatus.BAD_REQUEST,
+                "Invalid request field",
+                new ArrayList<>(Collections.singleton("eventGeneratedTimestamp: Cannot deserialize value of type `java.lang.Long` from Boolean value (token `JsonToken.VALUE_FALSE`)")));
+
+        mockMvc.perform(post("/registration/" + API_VERSION + "/gp2gpRegistrationStarted").content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException().getCause() instanceof MismatchedInputException))
+                .andExpect(content().json(asJsonString(expectedResponse)));
+    }
+
+    @Test
+    void shouldReturnA400IfJsonIsUnableParse() throws Exception {
+        var requestBody = "{";
+
+        ApiError expectedResponse = new ApiError(
+                HttpStatus.BAD_REQUEST,
+                "Invalid JSON",
+                new ArrayList<>(Collections.singleton("Unable to parse JSON")));
+
+        mockMvc.perform(post("/registration/" + API_VERSION + "/gp2gpRegistrationStarted").content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException().getCause() instanceof JsonParseException))
+                .andExpect(content().json(asJsonString(expectedResponse)));
+    }
+
+    @Test
+    void shouldReturnA400IfJsonIsUnableMapRequestIntoObject() throws Exception {
+        var requestBody = "{\"eventId\": \"}";
+
+        ApiError expectedResponse = new ApiError(
+                HttpStatus.BAD_REQUEST,
+                "Invalid JSON",
+                new ArrayList<>(Collections.singleton("eventId: Unexpected end-of-input in VALUE_STRING")));
+
+        mockMvc.perform(post("/registration/" + API_VERSION + "/gp2gpRegistrationStarted").content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException().getCause() instanceof JsonMappingException))
+                .andExpect(content().json(asJsonString(expectedResponse)));
+    }
+}
--- src/test/java/com/prmgpregistrationsmi/controller/registrationcontroller/RegistrationStartedEventTest.java
+++ src/test/java/com/prmgpregistrationsmi/controller/registrationcontroller/RegistrationStartedEventTest.java
@@ -1,31 +1,23 @@
 package com.prmgpregistrationsmi.controller.registrationcontroller;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.exc.MismatchedInputException;
-import com.prmgpregistrationsmi.controller.ApiError;
 import com.prmgpregistrationsmi.controller.RegistrationController;
-import com.prmgpregistrationsmi.model.*;
+import com.prmgpregistrationsmi.model.EventDAO;
+import com.prmgpregistrationsmi.model.EventResponse;
+import com.prmgpregistrationsmi.model.EventType;
+import com.prmgpregistrationsmi.model.RegistrationStartedEvent;
 import com.prmgpregistrationsmi.service.RegistrationService;
 import com.prmgpregistrationsmi.testhelpers.RegistrationStartedEventBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import static com.prmgpregistrationsmi.controller.RegistrationController.API_VERSION;
 import static com.prmgpregistrationsmi.utils.JsonHelper.asJsonString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -62,74 +54,6 @@ class RegistrationStartedEventTest {
         assertEquals("{\"eventId\":\"event-12345\"}", responseBody);
     }
 
-    @Test
-    void shouldReturn400RequestBodyIsEmpty() throws Exception {
-        RegistrationStartedEvent emptyRequestBody = new RegistrationStartedEvent();
-
-        ApiError expectedResponse = new ApiError(
-                HttpStatus.BAD_REQUEST,
-                "Failed to validate fields", List.of(
-                "reportingPracticeOdsCode: must not be empty",
-                "eventGeneratedTimestamp: must not be null",
-                "registrationId: must not be empty",
-                "payload: must not be null",
-                "reportingSystemSupplier: must not be empty",
-                "eventId: must not be empty"));
-
-        mockMvc.perform(post("/registration/" + API_VERSION + "/gp2gpRegistrationStarted").content(asJsonString(emptyRequestBody))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest())
-                .andExpect(result -> assertTrue(result.getResolvedException() instanceof MethodArgumentNotValidException))
-                .andExpect(content().json(asJsonString(expectedResponse)));
-    }
-
-    @Test
-    void shouldReturnA400IfEventGeneratedTimestampIsNotANumber() throws Exception {
-        var requestBody = "{\"eventId\":\"some-id\",\"eventGeneratedTimestamp\":" + false + "}";
-
-        ApiError expectedResponse = new ApiError(
-                HttpStatus.BAD_REQUEST,
-                "Invalid request field",
-                new ArrayList<>(Collections.singleton("eventGeneratedTimestamp: Cannot deserialize value of type `java.lang.Long` from Boolean value (token `JsonToken.VALUE_FALSE`)")));
-
-        mockMvc.perform(post("/registration/" + API_VERSION + "/gp2gpRegistrationStarted").content(requestBody)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest())
-                .andExpect(result -> assertTrue(result.getResolvedException().getCause() instanceof MismatchedInputException))
-                .andExpect(content().json(asJsonString(expectedResponse)));
-    }
-
-    @Test
-    void shouldReturnA400IfJsonIsUnableParse() throws Exception {
-        var requestBody = "{";
-
-        ApiError expectedResponse = new ApiError(
-                HttpStatus.BAD_REQUEST,
-                "Invalid JSON",
-                new ArrayList<>(Collections.singleton("Unable to parse JSON")));
-
-        mockMvc.perform(post("/registration/" + API_VERSION + "/gp2gpRegistrationStarted").content(requestBody)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest())
-                .andExpect(result -> assertTrue(result.getResolvedException().getCause() instanceof JsonParseException))
-                .andExpect(content().json(asJsonString(expectedResponse)));
-    }
-
-    @Test
-    void shouldReturnA400IfJsonIsUnableMapRequestIntoObject() throws Exception {
-        var requestBody = "{\"eventId\": \"}";
-
-        ApiError expectedResponse = new ApiError(
-                HttpStatus.BAD_REQUEST,
-                "Invalid JSON",
-                new ArrayList<>(Collections.singleton("eventId: Unexpected end-of-input in VALUE_STRING")));
-
-        mockMvc.perform(post("/registration/" + API_VERSION + "/gp2gpRegistrationStarted").content(requestBody)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest())
-                .andExpect(result -> assertTrue(result.getResolvedException().getCause() instanceof JsonMappingException))
-                .andExpect(content().json(asJsonString(expectedResponse)));
-    }
 
     @Test
     void shouldCallSaveEventWhenValidEventIsSent() throws Exception {
@@ -149,6 +73,5 @@ class RegistrationStartedEventTest {
                 .andExpect(status().isOk());
 
         verify(mockRegistrationService, times(1)).saveEvent(eq(testEvent), eq(EventType.GP2GP_REGISTRATION_STARTED));
-
     }
 }
