66fd38f2e11928a979a3780ee437f9dcc5040bfd
Serge Pogadaev <serge.pogadaev1@nhs.net>
2021-12-03 14:56:56 +0000

    PRMT-2447 [SP] Add integration tests for EHR Generated and EHR Requested events to make sure s3 upload is called

--- src/integrationtest/java/com/prmgpregistrationsmi/UploadEventToS3IntegrationTest.java
+++ src/integrationtest/java/com/prmgpregistrationsmi/UploadEventToS3IntegrationTest.java
@@ -1,11 +1,17 @@
 package com.prmgpregistrationsmi;
 
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.prmgpregistrationsmi.model.EhrGenerated.EhrGeneratedEvent;
+import com.prmgpregistrationsmi.model.EhrGenerated.EhrGeneratedPayload;
+import com.prmgpregistrationsmi.model.EhrRequested.EhrRequestedEvent;
+import com.prmgpregistrationsmi.model.EhrRequested.EhrRequestedPayload;
 import com.prmgpregistrationsmi.model.RegistrationStarted.RegistrationStartedEvent;
 import com.prmgpregistrationsmi.model.RegistrationStarted.RegistrationStartedPayload;
 import com.prmgpregistrationsmi.model.Event.EventDAO;
 import com.prmgpregistrationsmi.model.Event.EventResponse;
 import com.prmgpregistrationsmi.model.Event.EventType;
+import com.prmgpregistrationsmi.testhelpers.EhrGeneratedEventBuilder;
+import com.prmgpregistrationsmi.testhelpers.EhrRequestedEventBuilder;
 import com.prmgpregistrationsmi.testhelpers.RegistrationStartedEventBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +36,7 @@ class UploadEventToS3IntegrationTest {
     AmazonS3Client mockAmazonS3Client;
 
     @Test
-    void eventToS3Uploaded() {
+    void shouldUploadRegistrationStartedEventToS3WhenPostRequestIsMadeGivenValidEvent() {
         RegistrationStartedEvent registrationStartedEventRequest = RegistrationStartedEventBuilder
                 .withDefaultEventValues()
                 .eventId("event-id-test")
@@ -57,6 +63,83 @@ class UploadEventToS3IntegrationTest {
 
         EventResponse actualResponseEvent = restTemplate.postForObject("http://localhost:" + port +
                 "/registration/" + API_VERSION + "/gp2gpRegistrationStarted", registrationStartedEventRequest, EventResponse.class);
+
+        EventResponse expectedResponse = new EventResponse("event-id-test");
+        assertEquals(expectedResponse, actualResponseEvent);
+
+        verify(mockAmazonS3Client).putObject(
+                "test_bucket",
+                "v1/1970/01/04/15/event-id-test.json",
+                expectedS3UploadEvent.toString()
+        );
+    }
+
+    @Test
+    void shouldUploadEHRRequestedEventToS3WhenPostRequestIsMadeGivenValidEvent() {
+        EhrRequestedEvent ehrRequestedEventRequest = EhrRequestedEventBuilder
+                .withDefaultEventValues()
+                .eventId("event-id-test")
+                .eventGeneratedTimestamp(315130L)
+                .registrationId("registration-id-test-12345")
+                .reportingSystemSupplier("system-a")
+                .reportingPracticeOdsCode("A12345")
+                .build();
+
+        EhrRequestedPayload ehrRequestedPayload = EhrRequestedEventBuilder
+                .withDefaultEhrRequestedPayload()
+                .build();
+
+        EventDAO expectedS3UploadEvent = new EventDAO(
+                "event-id-test",
+                315130L,
+                EventType.EHR_REQUESTED,
+                "registration-id-test-12345",
+                "system-a",
+                "A12345",
+                ehrRequestedPayload
+        );
+
+        EventResponse actualResponseEvent = restTemplate.postForObject("http://localhost:" + port +
+                "/registration/" + API_VERSION + "/ehrRequested", ehrRequestedEventRequest, EventResponse.class);
+
+        EventResponse expectedResponse = new EventResponse("event-id-test");
+        assertEquals(expectedResponse, actualResponseEvent);
+
+        verify(mockAmazonS3Client).putObject(
+                "test_bucket",
+                "v1/1970/01/04/15/event-id-test.json",
+                expectedS3UploadEvent.toString()
+        );
+    }
+
+
+    @Test
+    void shouldUploadEHRGeneratedEventToS3WhenPostRequestIsMadeGivenValidEvent() {
+        EhrGeneratedEvent ehrGeneratedEventRequest = EhrGeneratedEventBuilder
+                .withDefaultEventValues()
+                .eventId("event-id-test")
+                .eventGeneratedTimestamp(315130L)
+                .registrationId("registration-id-test-12345")
+                .reportingSystemSupplier("system-a")
+                .reportingPracticeOdsCode("A12345")
+                .build();
+
+        EhrGeneratedPayload ehrGeneratedPayload = EhrGeneratedEventBuilder
+                .withDefaultEhrGeneratedPayload()
+                .build();
+
+        EventDAO expectedS3UploadEvent = new EventDAO(
+                "event-id-test",
+                315130L,
+                EventType.EHR_GENERATED,
+                "registration-id-test-12345",
+                "system-a",
+                "A12345",
+                ehrGeneratedPayload
+        );
+
+        EventResponse actualResponseEvent = restTemplate.postForObject("http://localhost:" + port +
+                "/registration/" + API_VERSION + "/ehrGenerated", ehrGeneratedEventRequest, EventResponse.class);
 
         EventResponse expectedResponse = new EventResponse("event-id-test");
         assertEquals(expectedResponse, actualResponseEvent);
