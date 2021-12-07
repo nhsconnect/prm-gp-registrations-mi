240b5317a8dce0b5530f48915334254d99c08158
K-Varsani <kavita.varsani1@nhs.net>
2021-12-07 10:02:25 +0000

    PRMT-2447 [SP/KV/IG] Replace mockbean to use bean in GP2GP controller test

--- src/test/java/com/prmgpregistrationsmi/controller/GP2GPControllerTest.java
+++ src/test/java/com/prmgpregistrationsmi/controller/GP2GPControllerTest.java
@@ -14,16 +14,15 @@ import com.prmgpregistrationsmi.testhelpers.RegistrationStartedEventBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 class GP2GPControllerTest {
-    @MockBean
+    @Mock
     private RegistrationService mockRegistrationService;
     private GP2GPController gp2gpController;
 
