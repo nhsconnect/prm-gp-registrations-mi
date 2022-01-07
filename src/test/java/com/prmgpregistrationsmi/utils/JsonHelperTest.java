82cff1ad84d1eafd9932f93e6093af4b6848d05c
Serge Pogadaev <serge.pogadaev1@nhs.net>
2022-01-07 10:25:16 +0000

    PRMT-2478 [DS/SP] Merge JsonHelperTest into one file

--- src/test/java/com/prmgpregistrationsmi/utils/JsonHelperTest.java
+++ src/test/java/com/prmgpregistrationsmi/utils/JsonHelperTest.java
@@ -1,9 +1,11 @@
 package com.prmgpregistrationsmi.utils;
 
+import com.prmgpregistrationsmi.model.Event.EventPayload.GPTransferMetadata;
 import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -28,5 +30,16 @@ class JsonHelperTest {
 
             JsonHelper.asJsonString(rectangle);
         });
+    }
+
+    @Test
+    void shouldConvertObjectWithDateTimeToJsonWithDateTimeString() {
+        Object testObject =  GPTransferMetadata.builder()
+                .conversationId("some-id")
+                .transferEventDateTime(LocalDateTime.of(2020, 2, 8, 8, 30))
+                .build();
+
+        assertEquals("{\"conversationId\":\"some-id\",\"transferEventDateTime\":\"2020-02-08T08:30:00\"}",
+                JsonHelper.asJsonString(testObject));
     }
 }
