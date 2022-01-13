5c78e3025172f409dc98620b4906ba7de2494175
Jake Newman <jacob.newman1@nhs.net>
2022-01-13 15:39:52 +0000

    PRMT-2388 [JN/AB] Log parameters as objects with data

--- src/main/java/com/prmgpregistrationsmi/model/StructuredLogEntry.java
+++ src/main/java/com/prmgpregistrationsmi/model/StructuredLogEntry.java
@@ -4,7 +4,6 @@ import lombok.Builder;
 import lombok.Getter;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.LogEvent;
-import org.apache.logging.log4j.message.Message;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -14,9 +13,9 @@ import java.time.format.DateTimeFormatter;
 @Getter
 @Builder
 public class StructuredLogEntry {
-    private String loggerName;
-    private Message message;
     private Level level;
+    private String message;
+    private Object[] data;
     private StackTraceElement source;
     private String timestamp;
 
@@ -28,8 +27,8 @@ public class StructuredLogEntry {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return StructuredLogEntry.builder()
                 .level(logEvent.getLevel())
-                .message(logEvent.getMessage())
-                .loggerName(logEvent.getLoggerName())
+                .message(logEvent.getMessage().getFormattedMessage())
+                .data(logEvent.getMessage().getParameters())
                 .source(logEvent.getSource())
                 .timestamp(dateTime.format(formatter))
                 .build();
--- src/test/java/com/prmgpregistrationsmi/model/StructuredLogEntryTest.java
+++ src/test/java/com/prmgpregistrationsmi/model/StructuredLogEntryTest.java
@@ -1,11 +1,16 @@
 package com.prmgpregistrationsmi.model;
 
+import com.prmgpregistrationsmi.model.gp2gp.EhrRequested.EhrRequestedEvent;
+import com.prmgpregistrationsmi.testhelpers.gp2gp.EhrRequestedEventBuilder;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.impl.Log4jLogEvent;
 import org.apache.logging.log4j.message.ObjectMessage;
+import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,7 +22,6 @@ class StructuredLogEntryTest {
         Log4jLogEvent expectedLog = new Log4jLogEvent().asBuilder()
                 .setLevel(Level.INFO)
                 .setMessage(new ObjectMessage("This is my fancy log entry"))
-                .setLoggerName("STRUCTURED_LOGGER")
                 .setSource(new StackTraceElement(
                         "classLoaderName",
                         null,
@@ -32,8 +36,25 @@ class StructuredLogEntryTest {
         StructuredLogEntry actualLog = StructuredLogEntry.getEntryFromLogEvent(expectedLog);
 
         assertEquals(expectedLog.getLevel(),actualLog.getLevel());
-        assertEquals(expectedLog.getMessage(),actualLog.getMessage());
+        assertEquals(expectedLog.getMessage().getFormattedMessage(),actualLog.getMessage());
         assertEquals(expectedLog.getSource(),actualLog.getSource());
         assertEquals("2022-01-13 11:25:51",actualLog.getTimestamp());
+    }
+
+    @Test
+    void shouldCreateStructuredLogEntryFromALogEventWithObject() {
+        EhrRequestedEvent testEvent = EhrRequestedEventBuilder
+                .withDefaultEventValues()
+                .build();
+
+        String expectedMessage = "Some message string";
+        Log4jLogEvent expectedLog = new Log4jLogEvent().asBuilder()
+                .setMessage(new ParameterizedMessage(expectedMessage,testEvent))
+                .build();
+
+        StructuredLogEntry actualLog = StructuredLogEntry.getEntryFromLogEvent(expectedLog);
+
+        assertEquals(expectedMessage,actualLog.getMessage());
+        assert(Arrays.equals(expectedLog.getMessage().getParameters(), actualLog.getData()));
     }
 }
