ddf5a493714e294683ab01ce673d8d4ce639c06a
Jake Newman <jacob.newman1@nhs.net>
2022-01-25 16:01:42 +0000

    PRMT-2388 [JN] Fix structured logger layout test by using param instead of object

--- src/test/java/com/prmgpregistrationsmi/logging/StructuredLoggerLayoutTest.java
+++ src/test/java/com/prmgpregistrationsmi/logging/StructuredLoggerLayoutTest.java
@@ -2,7 +2,7 @@ package com.prmgpregistrationsmi.logging;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.impl.Log4jLogEvent;
-import org.apache.logging.log4j.message.ObjectMessage;
+import org.apache.logging.log4j.message.ParameterizedMessage;
 import org.junit.jupiter.api.Test;
 
 import java.nio.charset.StandardCharsets;
@@ -16,7 +16,7 @@ class StructuredLoggerLayoutTest {
     void shouldConvertLogEventIntoStructuredLog() {
         Log4jLogEvent logEvent = new Log4jLogEvent().asBuilder()
                 .setLevel(Level.INFO)
-                .setMessage(new ObjectMessage("This is my fancy log entry"))
+                .setMessage(new ParameterizedMessage("This is my fancy log entry", "some object"))
                 .setSource(new StackTraceElement(
                         "classLoaderName",
                         null,
@@ -29,8 +29,8 @@ class StructuredLoggerLayoutTest {
                 .build();
 
         String structuredLog = structuredLoggerLayout.toSerializable(logEvent);
-        String expectedLog = "{\"level\":\"INFO\",\"message\":\"This is my fancy log entry\",\"data\":[\"This is my " +
-                "fancy log entry\"],\"source\":{\"classLoaderName\":\"classLoaderName\",\"moduleName\":null," +
+        String expectedLog = "{\"level\":\"INFO\",\"message\":\"This is my fancy log entry\"," +
+                "\"data\":\"some object\",\"source\":{\"classLoaderName\":\"classLoaderName\",\"moduleName\":null," +
                 "\"moduleVersion\":null,\"methodName\":\"shouldDoThing\",\"fileName\":\"TestFile.java\"," +
                 "\"lineNumber\":14,\"className\":\"declaringClass\",\"nativeMethod\":false}," +
                 "\"timestamp\":\"2022-01-13 11:25:51\"}\r\n";
