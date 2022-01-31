425ed9378a4499f8cd4f37a38801fd2211dccf25
Dorota-TW <dorota.sobkow1@nhs.net>
2022-01-31 14:50:40 +0000

    PRMT-2451 [AB/DS] Refactor gp2gp RegistrationStartedEventTest to only test the wiring of the payload

--- src/test/java/com/prmgpregistrationsmi/model/gp2gp/RegistrationStarted/RegistrationStartedEventTest.java
+++ src/test/java/com/prmgpregistrationsmi/model/gp2gp/RegistrationStarted/RegistrationStartedEventTest.java
@@ -4,8 +4,6 @@ import com.prmgpregistrationsmi.model.Event.EventPayload.RegistrationStarted;
 import com.prmgpregistrationsmi.testhelpers.RegistrationStartedBuilder;
 import com.prmgpregistrationsmi.testhelpers.gp2gp.RegistrationStartedEventBuilder;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
@@ -29,14 +27,26 @@ class RegistrationStartedEventTest {
     }
 
     @Test
-    void shouldThrowConstraintViolationWhenRegistrationStartedDateTimeIsNull() {
-        RegistrationStarted registrationPayload = RegistrationStartedBuilder
-                .withDefaultValues()
-                .registrationStartedDateTime(null)
+    void shouldThrowConstraintViolationWhenPayloadIsNull() {
+        RegistrationStartedEvent event = RegistrationStartedEventBuilder
+                .withDefaultEventValues()
+                .payload(null)
                 .build();
+
+        Set<ConstraintViolation<RegistrationStartedEvent>> violations = validator.validate(event);
+
+        assertEquals(1, violations.size());
+
+        ConstraintViolation<RegistrationStartedEvent> violation = violations.iterator().next();
+        assertEquals("must not be null", violation.getMessage());
+        assertEquals("payload", violation.getPropertyPath().toString());
+    }
+
+    @Test
+    void shouldThrowConstraintViolationWhenRegistrationStartedIsNull() {
         RegistrationStartedPayload payload = RegistrationStartedEventBuilder
                 .withDefaultRegistrationStartedPayload()
-                .registration(registrationPayload)
+                .registration(null)
                 .build();
         RegistrationStartedEvent event = RegistrationStartedEventBuilder
                 .withDefaultEventValues()
@@ -49,15 +59,14 @@ class RegistrationStartedEventTest {
 
         ConstraintViolation<RegistrationStartedEvent> violation = violations.iterator().next();
         assertEquals("must not be null", violation.getMessage());
-        assertEquals("payload.registration.registrationStartedDateTime", violation.getPropertyPath().toString());
+        assertEquals("payload.registration", violation.getPropertyPath().toString());
     }
 
-    @ParameterizedTest
-    @NullAndEmptySource
-    void shouldThrowConstraintViolationWhenRegistrationTypeIsNullOrEmpty(String registrationType) {
+    @Test
+    void shouldThrowConstraintViolationWhenRegistrationStartedFieldsAreInvalid() {
         RegistrationStarted payloadRegistration = RegistrationStartedBuilder
                 .withDefaultValues()
-                .registrationType(registrationType)
+                .registrationType(null)
                 .build();
         RegistrationStartedPayload payload = RegistrationStartedEventBuilder
                 .withDefaultRegistrationStartedPayload()
@@ -75,30 +84,5 @@ class RegistrationStartedEventTest {
         ConstraintViolation<RegistrationStartedEvent> violation = violations.iterator().next();
         assertEquals("must not be empty", violation.getMessage());
         assertEquals("payload.registration.registrationType", violation.getPropertyPath().toString());
-    }
-
-    @ParameterizedTest
-    @NullAndEmptySource
-    void shouldThrowConstraintViolationWhenRequestingPracticeOdsCodeIsNullOrEmpty(String requestingPracticeOdsCode) {
-        RegistrationStarted payloadRegistration = RegistrationStartedBuilder
-                .withDefaultValues()
-                .requestingPracticeOdsCode(requestingPracticeOdsCode)
-                .build();
-        RegistrationStartedPayload payload = RegistrationStartedEventBuilder
-                .withDefaultRegistrationStartedPayload()
-                .registration(payloadRegistration)
-                .build();
-        RegistrationStartedEvent event = RegistrationStartedEventBuilder
-                .withDefaultEventValues()
-                .payload(payload)
-                .build();
-
-        Set<ConstraintViolation<RegistrationStartedEvent>> violations = validator.validate(event);
-
-        assertEquals(1, violations.size());
-
-        ConstraintViolation<RegistrationStartedEvent> violation = violations.iterator().next();
-        assertEquals("must not be empty", violation.getMessage());
-        assertEquals("payload.registration.requestingPracticeOdsCode", violation.getPropertyPath().toString());
     }
 }
