81f65a0a5b769db9fa72062180d8e9e17f9ef0c9
Jake Newman <jacob.newman1@nhs.net>
2022-06-21 11:14:25 +0100

    PRMT-2678 [JN] Add StandardCharsets.UTF_8 to buildUUIDStringFromSeed use explicit charset

--- src/main/java/com/prmgpregistrationsmi/utils/UUIDService.java
+++ src/main/java/com/prmgpregistrationsmi/utils/UUIDService.java
@@ -1,9 +1,10 @@
 package com.prmgpregistrationsmi.utils;
 
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 public class UUIDService {
     public static String buildUUIDStringFromSeed(String seed) {
-        return UUID.nameUUIDFromBytes(seed.getBytes()).toString();
+        return UUID.nameUUIDFromBytes(seed.getBytes(StandardCharsets.UTF_8)).toString();
     }
 }
