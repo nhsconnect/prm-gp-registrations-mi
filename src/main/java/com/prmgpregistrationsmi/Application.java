25224347296d630e27f4dcf6e969f2a9a7bfc658
Jake Newman <jacob.newman1@nhs.net>
2022-09-09 17:11:20 +0100

    PRMT-2692 [JN] Add default security headers but disable requiring login

--- src/main/java/com/prmgpregistrationsmi/Application.java
+++ src/main/java/com/prmgpregistrationsmi/Application.java
@@ -2,10 +2,11 @@ package com.prmgpregistrationsmi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import static com.prmgpregistrationsmi.logging.StructuredLogger.logger;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
--- src/test/java/com/prmgpregistrationsmi/integrationtest/ApplicationIntegrationTest.java
+++ src/test/java/com/prmgpregistrationsmi/integrationtest/ApplicationIntegrationTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,5 +23,14 @@ class ApplicationIntegrationTest {
     void applicationIsHealthy() {
         assertThat(restTemplate.getForObject("http://localhost:" + port + "/_status",
                 String.class)).isEqualTo("{\"status\":\"UP\"}");
+    }
+
+    @Test
+    void respondsWithSecurityHeaders() {
+        HttpHeaders responseHeaders = restTemplate.headForHeaders("http://localhost:" + port + "/internalTransfer",
+                HttpHeaders.class);
+        assertThat(responseHeaders.getCacheControl()).isEqualTo("no-cache, no-store, max-age=0, must-revalidate");
+        assertThat(responseHeaders.getContentType()).isEqualTo(new MediaType(MediaType.APPLICATION_JSON));
+        assertThat(responseHeaders.get("X-XSS-Protection")).contains("1; mode=block");
     }
 }
